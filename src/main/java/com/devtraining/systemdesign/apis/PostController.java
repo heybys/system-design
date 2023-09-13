ccc789e10f348cc902ca8ca7363980e14bc32da1
heybys <byn0174@gmail.com>
2023-09-13 10:20:55 +0900

    add Slf4j to PostController

--- src/main/java/com/devtraining/systemdesign/apis/PostController.java
+++ src/main/java/com/devtraining/systemdesign/apis/PostController.java
@@ -1,10 +1,12 @@
 package com.devtraining.systemdesign.apis;
 
+import com.devtraining.systemdesign.post.domain.Post;
 import com.devtraining.systemdesign.post.service.PostInfo;
 import com.devtraining.systemdesign.post.service.PostService;
 import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/post")
